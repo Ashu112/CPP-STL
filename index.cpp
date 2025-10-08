148d4f0eea378a7ea73da9574d16d2c551c3365a
Ashu112 <kashu2611@gmail.com>
2025-10-08 16:47:06 +0530

    Cpp revision

--- index.cpp
+++ index.cpp
@@ -2,12 +2,24 @@
 
 using namespace std;
 
+// function with no return Type
+void print()
+{
+    cout << "Welcome Back to Exploring C++" << endl;
+}
+
+// function with return type as integer
+int sum(int a, int b)
+{
+    return a + b;
+}
+
 int main()
 {
-    pair<int, int> p;
+    // fuction call in c++
+    print();
 
-    // example of pairs.
-
-    cout << "Hello STL" << endl;
+    int result = sum(2, 5);
+    cout << "Result : " << result << endl;
     return 0;
 }
