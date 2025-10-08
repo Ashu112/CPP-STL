0299f6b487efa22c60348b2058f981ff121cc54a
Ashu112 <kashu2611@gmail.com>
2025-10-09 01:45:53 +0530

    stacks

---
+++ stack/stack.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void learnStack()
+{
+    // declaration of stack
+    stack<int> st;
+
+    // adding element in stack
+    st.push(2); // {2}
+    st.push(3); // {2,3}
+}
+
+int main()
+{
+    learnStack();
+    return 0;
+}
