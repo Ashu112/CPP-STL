b1af37ddf357267f7e3a78f8211fcf6976fe2d56
Ashu112 <kashu2611@gmail.com>
2025-10-09 23:43:54 +0530

    deque

---
+++ deque/deque.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void learnDequeue()
+{
+    // declaration
+    deque<int> dq;
+
+    // adding in deque
+    dq.push_back(3);    // {3}
+    dq.emplace_back(7); // {3,7}
+
+    // adding elemts in the front of deque
+    dq.push_front(10);    // {10,3,7}
+    dq.emplace_front(11); // {11,10,3,7}
+
+    for (auto it : dq)
+    {
+        cout << it << " ";
+    }
+    cout << endl;
+
+    // removing a element from back of  deque
+    dq.pop_back();
+
+    for (auto it : dq)
+    {
+        cout << it << " ";
+    }
+    cout << endl;
+
+    // removing a from front of deque
+    dq.pop_front();
+
+    for (auto it : dq)
+    {
+        cout << it << " ";
+    }
+    cout << endl;
+
+    // this gives the last element
+    cout << dq.back() << endl;
+
+    // this gives the first element
+    cout << dq.front();
+
+    // rest functions same as vector
+    // begin,end,rbegin , rend , clear , insert , size , swap
+}
+
+int main()
+{
+    learnDequeue();
+    return 0;
+}
