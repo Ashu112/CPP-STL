5b616c0d1e4878f363e7a1747740efd517d0d711
Ashu112 <kashu2611@gmail.com>
2025-10-17 08:09:33 +0530

    stakc

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -4,12 +4,35 @@ using namespace std;
 
 void learnStack()
 {
+    // stack work on LIFO -> Last in First out
     // declaration of stack
     stack<int> st;
 
     // adding element in stack
-    st.push(2); // {2}
-    st.push(3); // {2,3}
+    st.push(2);    // {2}
+    st.push(3);    // {2,3}
+    st.push(1);    // {2,3,1}
+    st.push(3);    // {2,3,1,3}
+    st.emplace(5); // {2,3,1,3,5}
+
+    // getting top element in a stack
+    cout << st.top() << endl; // prints 5 "** st[2] is invalid " stack do not have indexing
+
+    // removing element from stack
+    st.pop(); // removes 5 {2,3,1,3}
+
+    cout << st.top() << endl; //  3
+
+    // size of stack
+    cout << st.size() << endl; // 4
+
+    // checking if stack is empty or not
+    cout << st.empty() << endl; // false as stack is not empty
+
+    // swapping a stack
+    // this basically swaps the elemnts in the stack
+    // stack<int> s1, s2;
+    // s1.swap(s2);
 }
 
 int main()
